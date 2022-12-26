7d360a6329759fb59c4c0729e7ae4f23b6a0c5a1
Austin Campbell <austincamp@gmail.com>
2022-12-26 16:29:26 -0500

    updates to structure and created main.js

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+// when content is loaded run counter function
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const functionApi = "";
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json
+    }).then(response => {
+        console.log("called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    })
+    return count;
+}
