974140987edf08d597d91397e4750ec674431fe8
Austin Campbell <austincamp@gmail.com>
2022-12-26 20:39:23 -0500

    latest updates

---
+++ backend/api/GetResumeCounter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Company.Function
+{
+    public static class GetResumeCounter
+    {
+        [FunctionName("GetResumeCounter")]
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter, 
+            [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            ILogger log)
+        {
+            updatedCounter = counter;
+            updatedCounter.Count += 1;
+            var jsonToReturn = JsonConvert.SerializeObject(counter);
+
+            // debug
+            log.LogInformation("updatedCounter: " + updatedCounter.Count.ToString());
+            log.LogInformation("jsonToReturn: " + jsonToReturn);
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
--- frontend/main.js
+++ frontend/main.js
@@ -3,16 +3,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = "";
+const functionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
         return response.json
     }).then(response => {
-        console.log("called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
+        console.log(response.count);
+
     }).catch(function(error){
         console.log(error);
     })
