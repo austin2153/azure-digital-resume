4e7b3b06eade840922072eee3413ad3e874df126
Austin Campbell <austincamp@gmail.com>
2022-12-27 11:38:55 -0500

    created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,18 @@
 // when content is loaded run counter function
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
-})
+});
 
 const functionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
-        return response.json
+        return response.json()
     }).then(response => {
         count = response.count;
+        console.log("count: " + count);
         document.getElementById("counter").innerText = count;
-        console.log(response.count);
-
     }).catch(function(error){
         console.log(error);
     })
